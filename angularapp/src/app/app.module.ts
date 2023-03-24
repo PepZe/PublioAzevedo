c02b7c2df552450e1fe4dda56455627570e1d61a
PepZe <pedropaulo.azevedo@hotmail.com.br>
2023-03-24 20:21:55 +0000

    initial commit

---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule, MatToolbar } from '@angular/material/toolbar'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon'
+
+@NgModule({
+  exports: [MatSidenavModule, MatToolbarModule, MatToolbar],
+  declarations: [
+    AppComponent,
+    CoursesComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule, BrowserAnimationsModule, MatSidenavModule, MatToolbarModule, MatSlideToggleModule, MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
